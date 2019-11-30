17d644fddea21d41e4d321d35e3ccfabc38bd4a6
Shivam Patel <shivampatel066@gmail.com>
2019-11-30 12:43:26 +0530

    Reaction Based Game
    Reaction Based Game!! How Quick are You show the World !!

--- TrafficSignal_ObjC/ViewController.h
+++ TrafficSignal_ObjC/ViewController.h
@@ -8,7 +8,23 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    
+    NSTimer *timer;
+    NSTimer *scoreTimer;
+    
+    int timerInt;
+    int scoreInt;
+    
+    
+}
+
+
+@property (weak, nonatomic) IBOutlet UIImageView *trafficLight;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+
+- (IBAction)startGame:(id)sender;
 
 
 @end
--- TrafficSignal_ObjC/ViewController.m
+++ TrafficSignal_ObjC/ViewController.m
@@ -17,7 +17,58 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    scoreInt = 0;
+    self.scoreLabel.text = [NSString stringWithFormat:@"%i",scoreInt];
+    
 }
 
+
+- (IBAction)startGame:(id)sender {
+    
+    if (scoreInt == 0) {
+        
+        timerInt = 3;
+        timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(startTimer) userInfo:nil repeats:true];
+        self.scoreLabel.text = [NSString stringWithFormat:@"%i",scoreInt];
+        self.startButton.enabled = NO;
+        [self.startButton setTitle:@"Stop" forState:(UIControlStateNormal)];
+        
+    } else {
+        [scoreTimer invalidate];
+        [self.startButton setTitle:@"Restart" forState:(UIControlStateNormal)];
+    
+    }
+    if (timerInt == 0) {
+        scoreInt = 0;
+        timerInt = 3;
+    }
+}
+
+-(void)startTimer {
+    timerInt -= 1;
+    
+    if (timerInt == 2) {
+        self.trafficLight.image = [UIImage imageNamed:@"trafficLight3"];
+        
+    } else if (timerInt == 1) {
+        self.trafficLight.image = [UIImage imageNamed:@"trafficLight2"];
+    } else if (timerInt == 0) {
+        self.trafficLight.image = [UIImage imageNamed:@"trafficLight1"];
+        [timer invalidate];
+        scoreTimer = [NSTimer scheduledTimerWithTimeInterval:0.0001 target:self selector:@selector(scoreCounter) userInfo:nil repeats:true];
+        
+        [self.startButton setTitle:@"Stop" forState:UIControlStateNormal];
+        self.startButton.enabled = YES;
+    }
+     
+}
+
+-(void)scoreCounter {
+    
+    scoreInt += 1;
+    self.scoreLabel.text = [NSString stringWithFormat:@"%i",scoreInt];
+   
+    
+}
 
 @end
